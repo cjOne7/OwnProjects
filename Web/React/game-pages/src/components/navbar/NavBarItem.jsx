f54d1767ba6a8d0f97267858e3aeb43c90380c8f
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2022-06-10 00:10:42 +0200

    navbar

---
+++ Web/React/game-pages/src/components/navbar/NavBarItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBarItem = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NavBarItem;
--- Web/React/game-pages/src/components/navbar/NavigationBar.jsx
+++ Web/React/game-pages/src/components/navbar/NavigationBar.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import './navbar.css';
 
 const NavigationBar = () => {
     return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>MMORPG</li>
-                <li>Shooters</li>
-                <li>Hack&Slash</li>
-                <li>Contact us</li>
+        <nav className={'navbar-container'} role={"navigation"}>
+            <ul className={'navbar-list-items'}>
+                {/*<div className={'navbar-header'}>*/}
+                {/*    <li className={'list-element'}>*/}
+                {/*        <a className={'navbar-link'} href="#home">Home</a>*/}
+                {/*    </li>*/}
+                {/*</div>*/}
+                <a href="#home" className={'navbar-header'}>Home</a>
+                <div>
+                    <li className={'list-element'}>
+                        <a className={'navbar-link'} href="#mmorpg">MMORPG</a>
+                    </li>
+                </div>
+                <div>
+                    <li className={'list-element'}>
+                        <a className={'navbar-link'} href="#shooters">Shooters</a>
+                    </li>
+                </div>
+                <div>
+                    <li className={'list-element'}>
+                        <a className={'navbar-link'} href="#hackNslash">Hack&Slash</a>
+                    </li>
+                </div>
+                <div>
+                    <li className={'list-element'}>
+                        <a className={'navbar-link'} href="#contact">Contact us</a>
+                    </li>
+                </div>
             </ul>
-        </div>
+        </nav>
     );
 };
 
