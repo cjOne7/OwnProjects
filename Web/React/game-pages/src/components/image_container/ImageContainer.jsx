da8fc750f13271038ffba956b88a71c85102b7ec
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2022-07-12 01:06:20 +0200

    added vidoe as background

--- Web/React/game-pages/src/components/image_container/ImageContainer.jsx
+++ Web/React/game-pages/src/components/image_container/ImageContainer.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import './image.css';
+import './other.css'
 import ImageBox from "./ImageBox";
 import {gamesList} from "./image";
 
 const ImageContainer = ({genre}) => {
     return (
-        <div className={'image-container'}>
-            <div className={'image-box-wrapper'}>
-                {
-                    Object.values(gamesList[genre]).map((game, i) =>
-                        <ImageBox image={game} key={i}/>
-                    )
-                }
-            </div>
+        <div>
+            <section className={'showcase'}>
+                <div className={"btn"}>
+                    <div className={"link-about"}>
+                        <a href="#about">Read More</a>
+                    </div>
+                </div>
+                <div className="video-container">
+                    <video autoPlay muted loop
+                           // poster={require(`../../assets/background_videos/start_${genre}_bg_video.jpg`)}
+                           src={require(`../../assets/background_videos/${genre}_bg_video.mp4`)}/>
+                </div>
+                <div className={'image-container'}>
+                    <div className={'image-box-wrapper'}>
+                        {
+                            Object.values(gamesList[genre]).map((game, i) =>
+                                <ImageBox image={game} key={i}/>
+                            )
+                        }
+                    </div>
+                </div>
+            </section>
+            <section id="about">
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda atque culpa dicta doloribus
+                    enim est, eum
+                    fugit illo laboriosam maiores nihil numquam perferendis quasi sapiente, sint tempora tempore
+                    voluptatem?</p>
+            </section>
         </div>
     );
 };
