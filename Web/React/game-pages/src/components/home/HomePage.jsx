39469d71c763736a8397995b6523ab1c4dd08941
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2022-07-11 14:01:01 +0200

    added home content

--- Web/React/game-pages/src/components/home/HomePage.jsx
+++ Web/React/game-pages/src/components/home/HomePage.jsx
@@ -1,81 +1,10 @@
 import React from 'react';
+import './home_page_content.css'
 
 const HomePage = () => {
     return (
-        <div style={{textAlign: "center", width: "80%", margin: "auto"}}>
-            <h1>Home page</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
-                excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
-                nisi perferendis porro sed sit voluptatum.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis consequuntur cum ducimus
-                exercitationem harum incidunt iure, libero neque odio pariatur perferendis ut. Dolore molestiae porro
-                rem sequi ullam vero.</p>
+        <div className={'home-container'}>
+            <h1 className={'home-header'}>To see the content, please, use navbar menu</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta doloremque dolores ex
                 excepturi ipsam quaerat ratione reprehenderit, tempora temporibus. Architecto assumenda eos expedita
                 nisi perferendis porro sed sit voluptatum.
